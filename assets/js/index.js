f5fb63d5e18279585c0833c9378117f2ac0e4190
Zahid Shaikh <zahid.786shaikh@gmail.com>
2024-12-07 19:36:32 +0530

    feat: initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        "pure-white": "#ffffff",
+        cream: "#F2EAE2",
+        "aurometal-saurus": "#6C7289",
+        "deep-aquamarine": "#3D8168",
+        "deep-aquamarine-hovered": "#1A4032",
+        gunmetal: "#1C232B",
+      },
+    },
+  },
+};
